979b25cb6824d6a4bd8436fe1134788bfc37de62
John Wu <jywu86@gmail.com>
2022-04-29 20:48:07 -0700

    Adding Modeling Data and Modeling Data R script.  Update to F1-Project write-up.

---
+++ Modeling_Data_Creation.R
@@ -0,0 +1,82 @@
+library(dplyr)
+library(tidyverse)
+
+# Reading in data
+results <- read.csv('results.csv')
+races <- read.csv('Relevant_Races.csv')
+driver_id <- read.csv('drivers.csv')
+circuits <- read.csv('Relevant_Circuits_Final.csv')
+qualify <- read.csv('qualifying.csv')
+r_qualify <- read.csv('Relevant_Qualify_2.csv')
+r_qualify <- select(r_qualify, -X.1, -X) # removing X.1 and X columns
+
+# Removing columns from results that are not needed
+results <- results[,c('resultId','raceId','driverId',
+                      'constructorId','number','grid',
+                      'position','positionText','positionOrder','time')]
+
+
+
+# Merging data and combining columns 
+r_races <- races[,c('year','raceId','circuitId','Air.Temp','Track.Temp','Rainfall','Wind.Speed')] # creating a reference dataframe
+final_results <- merge(x=results, y=r_races, by='raceId',all.y=TRUE) 
+
+# Merging Final Results with Driver Name
+#driver_id <- driver_id[,c('driverId','driverRef')]
+#final_results <- merge(x =driver_id, y = final_results, by = 'driverId', all.y=TRUE)
+
+#race_id <- r_races[,c('raceId','year')]
+
+# Adding track_group (track speed) to results
+circuits <- transform(circuits, track_group = ifelse(Avg_Speed_MPH<127.6, 'Low_Speed',ifelse(Avg_Speed_MPH<152.4, 'Med_Speed','High_Speed')))
+speed_cols <- circuits[,c('circuitId','track_group')]
+final_results <- merge(x=final_results, y=speed_cols, by='circuitId',all.x=TRUE)
+
+# Filtering out only relevant qualifying data
+qualify_filter <- r_qualify %>% distinct(raceId)
+final_results <- merge(x=final_results, y = qualify_filter, by.x='raceId',all.y=TRUE)
+
+# Removing results where people DQ'ed or Retired
+final_results <- filter(final_results, positionText != 'R')
+
+# Finding Top 10 drivers
+driver_group <- final_results %>% group_by(driverId) %>% summarise(n=n()) %>%  arrange(desc(n))
+driver_group <- merge(x=driver_group, y= driver_id, by='driverId',all.x=TRUE)
+driver_group <- driver_group %>% arrange(desc(driver_group$n))
+
+# Filtering by Top 8 Drivers (with most races) and merging with Driver Names
+affected_drivers <- driver_group[1:10,c('driverId','driverRef')] # filtering top 8 drivers
+final_results <- merge(x=final_results, y=affected_drivers, by='driverId',all.y=TRUE) # filtering out only results with top 8 drivers
+
+# Creating points system for positions 1-22 (Maybe not needed)
+#points_model <- c(22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1)
+#positionOrder <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22)
+#position_df <- data.frame(positionOrder,points_model)
+#final_results <- merge(x=final_results, y=position_df, by='positionOrder',all.x=TRUE) # adding points to modeling data
+
+# Creating team_rank by finding fastest average qualifying
+team_rank <- r_qualify[,c('raceId','position_team','constructorId')]
+team_rank <- team_rank %>% distinct(raceId,constructorId, .keep_all=TRUE)
+
+race_rank <- team_rank %>% 
+  arrange(raceId, constructorId, position_team) %>% 
+  group_by(raceId) %>% 
+  mutate(team_rank = rank(position_team))
+
+race_rank <- race_rank[,c('team_rank','raceId','constructorId')]
+
+final_results <- merge(x=final_results,y=race_rank, by=c('constructorId','raceId'),all.x=TRUE)
+
+final_results <- final_results %>% 
+  arrange(raceId, constructorId, team_rank) %>% 
+  group_by(raceId) %>% 
+  mutate(team_rank = rank(team_rank, ties.method='min'))
+
+final_results$position <- as.numeric(final_results$position)
+
+final_results <- final_results %>% 
+  arrange(raceId, driverId,position) %>% 
+  group_by(raceId) %>% 
+  mutate(final_position = rank(position, ties.method='min'))
+
+write.csv(final_results,'Modeling_Data.csv', row.names=FALSE)
