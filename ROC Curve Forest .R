a9a10e4d67a68a10bd42b822d371d195e99f5373
John Wu <jywu86@gmail.com>
2022-05-03 15:46:14 -0700

    ROC Curve Forest

--- ROC Curve Forest .R
+++ ROC Curve Forest .R
@@ -12,6 +12,7 @@ mydata_all <- read.csv('Model_Data_w_Turns.csv')
 mydata_win <- subset(mydata_all, select= c(dist.mi,grid,position,Air.Temp,Track.Temp,Wind.Speed,driverRef,constructorRef,Rainfall2,qualifying_dif,team_rank,dist_turns,dist_s_turns,turns_mile,turns_s_mile,Turns,Sharp.Turns,Type))
 mydata_win$win[mydata_win$position ==1] <- 'Win'
 mydata_win$win[mydata_win$position !=1] <- 'Lose'
+mydata_win <- na.omit(mydata_win)
 
 #################### Data Cleaning and dropping columns ###############################
 
@@ -41,15 +42,57 @@ forest_win <- train(win ~.,
 
 # Create initial probability predictions based on test
 forest_win_pred <- predict(forest_win, test_win, type='prob')
+
 # ROC curve for random forest
 rforest <- multiclass.roc(test_win$win,forest_win_pred$Win, percent=TRUE)
 roc_forest <- rforest[['rocs']]
 r_forest <-roc_forest[[1]]
 plot.roc(r_forest,
-         col = 'red',
+         #col = 'red',
          print.auc = T,
+         print.auc.cex = 1.1,
          auc.polygon = T,
          max.auc.polygon = T,
          auc.polygon.col ='lightgoldenrod',
          print.thres = T,
+         print.thres.cex=1.05,
          main = 'ROC Curve for Forest')
+
+coords(r_forest,"best", ret='threshold',transpose=FALSE)  # <----- This gives you the actual threshold value
+
+# Prediction using ROC threshold
+forest_win_factor <- as.factor(ifelse(forest_win_pred$Win>0.0765,"Win","Lose")) 
+
+# Confusion Matrix 
+confusionMatrix(forest_win_factor, test_win$win, positive='Win')
+
+plot.roc(r_log,
+         print.auc = T,
+         print.auc.cex=1.1,
+         auc.polygon = T,
+         max.auc.polygon = T,
+         auc.polygon.col ='lightblue',
+         print.thres = T,
+         print.thres.cex=1.05,
+         main = 'ROC Curve for Logistic (Win)')
+plot.roc(r_forest,
+         print.auc = T,
+         print.auc.cex=1.1,
+         auc.polygon = T,
+         max.auc.polygon = T,
+         auc.polygon.col ='lightgoldenrod',
+         print.thres = T,
+         print.thres.cex=1.05,
+         main = 'ROC Curve for Forest (Win)')
+plot.roc(r_boost,
+         print.auc = T,
+         print.auc.cex=1.1,
+         auc.polygon = T,
+         max.auc.polygon = T,
+         auc.polygon.col ='Tomato',
+         print.thres = T,
+         print.thres.cex=1.05,
+         main = 'ROC Curve for Boost (Win)')
+
+
+
